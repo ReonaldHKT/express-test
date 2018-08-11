eda5105dd8577e0c1afb2264bbc853059de2c3aa
ReonaldHKT <reonaldhkt+webs@gmail.com>
2018-08-12 00:48:30 +0900

    fix index.js 2

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 var app = express();
 
-express.mime.type['css'] = 'text/css';
+express.static.mime.define({'text/css': ['css']});
 router.use(express.static(path.join(__dirname, '/public'), {
   setHeaders: function(res, path, stat){
     res.set('X-Content-Type-Options', 'nosniff');
